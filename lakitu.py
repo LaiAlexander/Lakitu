40f17424f29cd0e5a87041d98cf0cd8cdaddad9a
Lai Alexander <laialexanderholmsen@gmail.com>
2021-03-01 12:09:51 +0100

    Update gitignore. Will list multiple servers at startup if connected to more than one.

--- lakitu.py
+++ lakitu.py
@@ -5,27 +5,27 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-# import leaderboard
-
+# Setup
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD = os.getenv("GUILD_TOKEN")
-
 INTENTS = discord.Intents.all()
 BOT = commands.Bot(command_prefix="!", intents=INTENTS)
+
 # Load extensions
 COGS = [file.stem for file in Path.cwd().joinpath("cogs").glob("**/*.py")]
 for cog in COGS:
     BOT.load_extension(f"cogs.{cog}")
 
+
 @BOT.event
 async def on_ready():
-    guild = discord.utils.get(BOT.guilds, name=GUILD)
-    print(
-        f'{BOT.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})\n-----'
-    , flush=True)
+    print(f"{BOT.user} is connected to the following guild(s):", flush=True)
+    for guild in BOT.guilds:
+        print(f"{guild.name}: (id: {guild.id})", flush=True)
+    print("-----", flush=True)
     activity = discord.Activity(type=discord.ActivityType.watching, name="MK8D racers!")
     await BOT.change_presence(activity=activity)
 
-BOT.run(TOKEN)
+
+if __name__ == "__main__":
+    BOT.run(TOKEN)
